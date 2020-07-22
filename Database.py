9b6b725cbdfc79c469290c058783f8b3232e315c
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-22 00:39:05 -0400

    Created /project route to add new projects to database

--- Database.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-def database(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/project_planner'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = SQLAlchemy(app)
-
-    return db
