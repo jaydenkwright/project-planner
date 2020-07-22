9ebad470fc42119d55a5a1f9fe8dcf0bd15c1eef
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-21 23:35:38 -0400

    Created Project database Model

---
+++ Project.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from datetime import datetime
+from main import db
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    category = db.Column(db.String(100), nullable=False)
+    dueDate = db.Column(db.DateTime)
+    github = db.Column(db.String())
+    figma = db.Column(db.String())
+    privacy = db.Column(db.Boolean())
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, id, name, description, category, dueDate, github, figma, privacy):
+        self.userId = userId
+        self.name = name 
+        self.description = description
+        self.category = category
+        self.dueDate = dueDate
+        self.github = github
+        self.figma = figma
+        self.privacy = privacy
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
-api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flask_test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
