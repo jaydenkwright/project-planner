7160bf0caa08573da18f738a30eca0fb7676a608
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-22 18:19:36 -0400

    Created model for Project phase database

---
+++ PhaseModel.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from config import db, marsh
+
+class Phase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer)
+    projectId = db.Column(db.Integer)
+    phaseNumber = db.Column(db.Integer)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    category = db.Column(db.String(100), nullable=False)
+    dueDate = db.Column(db.DateTime)
+    completed = db.Column(db.Boolean())
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, userId, projectId, phaseNumber, name, description, category, dueDate, completed):
+        self.userId = userId
+        self.projectId = projectId
+        self.phaseNumber = phaseNumber
+        self.name = name
+        self.description = description
+        self.category = category
+        self.dueDate = dueDate
+        self.completed = completed
+
+class PhaseSchema(marsh.Schema):
+    class Meta:
+        fields = ('id',
+        'userId',
+        'projectId',
+        'phaseNumber',
+        'name',
+        'description',
+        'category',
+        'dueDate',
+        'completed')
+
+phase_schema = PhaseSchema()
+phases_schema = PhaseSchema(many=True)
