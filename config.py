95f706d8617d1f96d909e08118edd54d9bbcc71a
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-22 15:20:01 -0400

    Fixed config file

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from settings.settings import DATABASE
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/project_planner'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 marsh = Marshmallow(app)
