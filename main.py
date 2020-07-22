786046e772b47da2dff56ac52d70a42936df6239
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-22 16:44:11 -0400

    Moved routes to seperate file

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from datetime import datetime
 from ProjectModel import Project, project_Schema, projects_Schema 
 from config import db, app
-
-
-@app.route('/project', methods=['POST'])
-def addProject():
-    userId = request.json['userId']
-    name = request.json['name']
-    description = request.json['description']
-    category = request.json['category']
-    dueDate = request.json['dueDate'] 
-    github = request.json['github']
-    figma = request.json['figma']
-    privacy = request.json['privacy']
-
-    project = Project(userId, name, description, category, dueDate, github, figma, privacy)
-    db.session.add(project)
-    db.session.commit()
-
-    return project_Schema.jsonify(project)
-
+import ProjectRoute
 
 if __name__ == "__main__":
     app.run(debug=True)
