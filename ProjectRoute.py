3ef154e43814cbbbf08b592cf0fa5fce5a851fbc
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-22 17:17:00 -0400

    Created /project/<id> for PUT requests to update projects

--- ProjectRoute.py
+++ ProjectRoute.py
@@ -25,3 +25,28 @@ def addProject():
 def getProject(id):
     project = Project.query.get(id)
     return project_Schema.jsonify(project)
+
+@app.route('/project/<id>', methods=['PUT'])
+def updateProject(id):
+    project = Project.query.get(id)
+    userId = request.json['userId']
+    name = request.json['name']
+    description = request.json['description']
+    category = request.json['category']
+    dueDate = request.json['dueDate'] 
+    github = request.json['github']
+    figma = request.json['figma']
+    privacy = request.json['privacy']
+
+    project.userId = userId
+    project.name = name
+    project.description = description
+    project.category = category
+    project.dueDate = dueDate
+    project.github = github
+    project.figma = figma
+    project.privacy = privacy
+
+    db.session.commit()
+
+    return project_Schema.jsonify(project)
