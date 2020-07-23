eb00a08c6b95c26df034a620e94bff2547e7e74a
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-23 17:26:24 -0400

    Added verify decorator to TaskRoute.py

--- TaskRoute.py
+++ TaskRoute.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from TaskModel import Task, task_schema, tasks_schema 
+from verify import verify
 from config import db, app
 
 # Create new task
 @app.route('/task/add', methods=['POST'])
-def addTask():
+@verify
+def addTask(user):
     userId = request.json['userId']
     phaseId = request.json['phaseId']
     taskText = request.json['taskText']
@@ -24,13 +26,15 @@ def getTask(id):
 
 # Get all tasks from certain phase
 @app.route('/tasks/<phaseId>', methods=['GET'])
-def getTasks(phaseId):
+@verify
+def getTasks(user, phaseId):
     task = Task.query.filter(Task.phaseId == phaseId).all()
     return tasks_schema.jsonify(task)
 
 # Update task
 @app.route('/task/update/<id>', methods=['PUT'])
-def updateTask(id):
+@verify
+def updateTask(user, id):
     task = Task.query.get(id)
     userId = request.json['userId']
     phaseId = request.json['phaseId']
@@ -47,7 +51,8 @@ def updateTask(id):
 
 # Delete tasks
 @app.route('/task/delete/<id>', methods=['DELETE'])
-def deleteTask(id):
+@verify
+def deleteTask(user, id):
     task = Task.query.get(id)
     db.session.delete(task)
     db.session.commit()
