ec7230f46d7d04e7564aa99d975057130261aacb
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-22 20:09:12 -0400

    Create routes to Create new phases and retrieve them

---
+++ PhaseRoute.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from PhaseModel import Phase, phase_schema, phases_schema 
+from config import db, app
+
+# Create new phase
+@app.route('/phase', methods=['POST'])
+def addPhase():
+    userId = request.json['userId']
+    projectId = request.json['projectId']
+    phaseNumber = request.json['phaseNumber']
+    name = request.json['name']
+    description = request.json['description']
+    category = request.json['category']
+    dueDate = request.json['dueDate']
+    completed = request.json['completed']
+
+    phase = Phase(userId, projectId, phaseNumber, name, description, category, dueDate, completed)
+    db.session.add(phase)
+    db.session.commit()
+
+    return phase_schema.jsonify(phase)
+
+# Get specific phase
+@app.route('/phase/<id>', methods=['GET'])
+def getPhase(id):
+    phase = Phase.query.get(id)
+    return phase_schema.jsonify(phase)
+
+# Get all phases from certain user
+@app.route('/phases/<userId>', methods=['GET'])
+def getUserPhases(userId):
+    phase = Phase.query.filter(Phase.userId == userId).all()
+    return phases_schema.jsonify(phase)
