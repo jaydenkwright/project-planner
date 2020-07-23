59cb8fe1a6d09a183a7d2da45c3d50a1aedf0df0
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-23 01:04:39 -0400

    fixed error by importing app

--- verify.py
+++ verify.py
@@ -1,19 +1,19 @@
+from flask import Flask, request
 import jwt
 from UserModel import User, user_schema
 from functools import wraps 
+from config import app
 
 
 def verify(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.cookies.get('token')
-
         if not token:
             return {"error": "User is not logged in."}
     
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'])
-            print(data['userId'])
             current_user = User.query.filter(User.userId == data['userId']).first()
         except:
             return {'error': 'User token is invalid'}
