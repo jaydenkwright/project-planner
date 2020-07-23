64dd66b44750cff68551000f638ef7176d55fbc6
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-22 21:51:30 -0400

    created /task/update/<id> route to update tasks

--- TaskRoute.py
+++ TaskRoute.py
@@ -15,3 +15,32 @@ def addTask():
     db.session.commit()
 
     return task_schema.jsonify(task)
+
+# Get specific task
+@app.route('/task/<id>', methods=['GET'])
+def getTask(id):
+    task = Task.query.get(id)
+    return task_schema.jsonify(task)
+
+# Get all tasks from certain phase
+@app.route('/tasks/<phaseId>', methods=['GET'])
+def getTasks(phaseId):
+    task = Task.query.filter(Task.phaseId == phaseId).all()
+    return tasks_schema.jsonify(task)
+
+# Update task
+@app.route('/task/update/<id>', methods=['PUT'])
+def updateTask(id):
+    task = Task.query.get(id)
+    userId = request.json['userId']
+    phaseId = request.json['phaseId']
+    taskText = request.json['taskText']
+    completed = request.json['completed']
+
+    task.userId = userId
+    task.phaseId = phaseId
+    task.taskText = taskText
+    task.completed = completed
+
+    db.session.commit()
+    return task_schema.jsonify(task)
