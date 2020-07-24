dd931283ff69374ab39f626b764c31623adcda75
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-24 18:45:36 -0400

    Added try and except blocks to catch errors

--- UserRoute.py
+++ UserRoute.py
@@ -9,52 +9,64 @@ import datetime
 
 @app.route('/register', methods=['POST'])
 def register():
-    userId = str(uuid.uuid4())
-    firstName = request.json['firstName']
-    lastName = request.json['lastName']
-    email = request.json['email']
-    password = request.json['password']
-    hashed_password = generate_password_hash(password, method='sha256')
-    user = User(userId, firstName, lastName, email, hashed_password)
-    db.session.add(user)
-    db.session.commit()
+    try:
+        userId = str(uuid.uuid4())
+        firstName = request.json['firstName']
+        lastName = request.json['lastName']
+        email = request.json['email']
+        password = request.json['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        user = User(userId, firstName, lastName, email, hashed_password)
+        db.session.add(user)
+        db.session.commit()
 
-    return {
-        'msg': 'User created'
-    }
+        return {
+            'msg': 'User created'
+        }
+    except:
+        raise AssertionError('Something went wrong')
 
 @app.route('/login', methods=['POST'])
 def login():
-    auth = request.authorization
-    email = auth.username
-    password = auth.password
-    if not auth or not email or not password:
-        raise AssertionError('Missing email or password')
-    
-    user = User.query.filter(User.email == email).first()
-    if not user:
+    try:
+        auth = request.authorization
+        email = auth.username
+        password = auth.password
+        if not auth or not email or not password:
+            raise AssertionError('Missing email or password')
+        
+        user = User.query.filter(User.email == email).first()
+        if not user:
+            raise AssertionError('Incorrect email or password')
+
+        if check_password_hash(user.password, password):
+            token = jwt.encode({'userId': user.userId, 
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30)},
+            app.config['SECRET_KEY']
+            )
+            resp = make_response('')
+            resp.set_cookie('token', token.decode('UTF-8'), httponly = True)
+            return resp
+
         raise AssertionError('Incorrect email or password')
-
-    if check_password_hash(user.password, password):
-        token = jwt.encode({'userId': user.userId, 
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30)},
-        app.config['SECRET_KEY']
-        )
-        resp = make_response('')
-        resp.set_cookie('token', token.decode('UTF-8'), httponly = True)
-        return resp
-
-    raise AssertionError('Incorrect email or password')
+    except:
+        raise AssertionError('Something went wrong')
     
 
 @app.route('/user', methods=['GET'])
 @verify
 def getCurrentUser(currentUser):
-    user = User.query.filter(User.userId == currentUser.userId).first()
-    return get_user_schema.jsonify(user)
+    try:
+        user = User.query.filter(User.userId == currentUser.userId).first()
+        return get_user_schema.jsonify(user)
+    except:
+        raise AssertionError('Something went wrong')
 
 @app.route('/user/<id>', methods=['GET'])
 @verify
 def getUser(user, id):
-    user = User.query.get(id)
-    return get_user_schema.jsonify(user)
+    try:
+        user = User.query.get(id)
+        return get_user_schema.jsonify(user)
+    except:
+        raise AssertionError('Something went wrong')
