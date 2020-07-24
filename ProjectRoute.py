022f7cff974895fc8113826b39dde8915866b41d
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-24 18:49:13 -0400

    Added try and except blocks to catch errors

--- ProjectRoute.py
+++ ProjectRoute.py
@@ -8,67 +8,82 @@ from config import db, app
 @app.route('/project/add', methods=['POST'])
 @verify
 def addProject(user):
-    userId = user.userId
-    name = request.json['name']
-    description = request.json['description']
-    category = request.json['category']
-    dueDate = request.json['dueDate'] 
-    github = request.json['github']
-    figma = request.json['figma']
-    privacy = request.json['privacy']
+    try:
+        userId = user.userId
+        name = request.json['name']
+        description = request.json['description']
+        category = request.json['category']
+        dueDate = request.json['dueDate'] 
+        github = request.json['github']
+        figma = request.json['figma']
+        privacy = request.json['privacy']
 
-    project = Project(userId, name, description, category, dueDate, github, figma, privacy)
-    db.session.add(project)
-    db.session.commit()
+        project = Project(userId, name, description, category, dueDate, github, figma, privacy)
+        db.session.add(project)
+        db.session.commit()
 
-    return project_Schema.jsonify(project)
+        return project_Schema.jsonify(project)
+    except:
+        raise AssertionError('Something went wrong')
 
 # Get all projects created by a certain user
 @app.route('/projects', methods=['GET'])
 @verify
 def getUserProjects(user):
-    projects = Project.query.filter(Project.userId == user.userId).all()
-    return projects_Schema.jsonify(projects)
+    try:
+        projects = Project.query.filter(Project.userId == user.userId).all()
+        return projects_Schema.jsonify(projects)
+    except:
+        raise AssertionError('Something went wrong')
 
 # Get a specific project with a given id
 @app.route('/project/<id>', methods=['GET'])
 def getProject(id):
-    project = Project.query.get(id)
-    return project_Schema.jsonify(project)
+    try:
+        project = Project.query.get(id)
+        return project_Schema.jsonify(project)
+    except:
+        raise AssertionError('Something went wrong')
 
 # Update a project
 @app.route('/project/update/<id>', methods=['PUT'])
 @verify
 def updateProject(user, id):
-    project = Project.query.get(id)
-    userId = user.userId
-    name = request.json['name']
-    description = request.json['description']
-    category = request.json['category']
-    dueDate = request.json['dueDate'] 
-    github = request.json['github']
-    figma = request.json['figma']
-    privacy = request.json['privacy']
+    try:
+        project = Project.query.get(id)
+        userId = user.userId
+        name = request.json['name']
+        description = request.json['description']
+        category = request.json['category']
+        dueDate = request.json['dueDate'] 
+        github = request.json['github']
+        figma = request.json['figma']
+        privacy = request.json['privacy']
 
-    project.userId = userId
-    project.name = name
-    project.description = description
-    project.category = category
-    project.dueDate = dueDate
-    project.github = github
-    project.figma = figma
-    project.privacy = privacy
+        project.userId = userId
+        project.name = name
+        project.description = description
+        project.category = category
+        project.dueDate = dueDate
+        project.github = github
+        project.figma = figma
+        project.privacy = privacy
 
-    db.session.commit()
+        db.session.commit()
 
-    return project_Schema.jsonify(project)
+        return project_Schema.jsonify(project)
+    except:
+        raise AssertionError('Something went wrong')
 
 # Delete a project
 @app.route('/project/delete/<id>', methods=['DELETE'])
 @verify
 def deleteProject(user, id):
-    project = Project.query.get(id)
-    db.session.delete(project)
-    db.session.commit()
+    try:
+        project = Project.query.get(id)
+        db.session.delete(project)
+        db.session.commit()
 
-    return project_Schema.jsonify(project)
+        return project_Schema.jsonify(project)
+    except:
+        raise AssertionError('Something went wrong')
