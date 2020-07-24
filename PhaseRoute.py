e44608e9bb1d9f25f434cecff6dabad196e7df3d
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-24 18:46:50 -0400

    Added try and except blocks to catch errors

--- PhaseRoute.py
+++ PhaseRoute.py
@@ -7,64 +7,79 @@ from config import db, app
 @app.route('/phase/add', methods=['POST'])
 @verify
 def addPhase(user):
-    userId = user.userId
-    projectId = request.json['projectId']
-    phaseNumber = request.json['phaseNumber']
-    name = request.json['name']
-    description = request.json['description']
-    category = request.json['category']
-    dueDate = request.json['dueDate']
-    completed = False
+    try:
+        userId = user.userId
+        projectId = request.json['projectId']
+        phaseNumber = request.json['phaseNumber']
+        name = request.json['name']
+        description = request.json['description']
+        category = request.json['category']
+        dueDate = request.json['dueDate']
+        completed = False
 
-    phase = Phase(userId, projectId, phaseNumber, name, description, category, dueDate, completed)
-    db.session.add(phase)
-    db.session.commit()
+        phase = Phase(userId, projectId, phaseNumber, name, description, category, dueDate, completed)
+        db.session.add(phase)
+        db.session.commit()
 
-    return phase_schema.jsonify(phase)
+        return phase_schema.jsonify(phase)
+    except:
+        raise AssertionError('Something went wrong')
 
 # Get specific phase
 @app.route('/phase/<id>', methods=['GET'])
 def getPhase(id):
-    phase = Phase.query.get(id)
-    return phase_schema.jsonify(phase)
+    try:
+        phase = Phase.query.get(id)
+        return phase_schema.jsonify(phase)
+    except:
+        raise AssertionError('Something went wrong')
 
 # Get all phases from certain user
 @app.route('/phases', methods=['GET'])
 @verify
 def getUserPhases(user):
-    phase = Phase.query.filter(Phase.userId == user.userId).all()
-    return phases_schema.jsonify(phase)
+    try:
+        phase = Phase.query.filter(Phase.userId == user.userId).all()
+        return phases_schema.jsonify(phase)
+    except:
+        raise AssertionError('Something went wrong')
 
 @app.route('/phase/update/<id>', methods=['PUT'])
 @verify
 def updatePhase(user, id):
-    phase = Phase.query.get(id)
-    userId = user.userId
-    projectId = request.json['projectId']
-    phaseNumber = request.json['phaseNumber']
-    name = request.json['name']
-    description = request.json['description']
-    category = request.json['category']
-    dueDate = request.json['dueDate']
-    completed = request.json['completed']
+    try:
+        phase = Phase.query.get(id)
+        userId = user.userId
+        projectId = request.json['projectId']
+        phaseNumber = request.json['phaseNumber']
+        name = request.json['name']
+        description = request.json['description']
+        category = request.json['category']
+        dueDate = request.json['dueDate']
+        completed = request.json['completed']
 
-    phase.userId = userId
-    phase.projectId = projectId
-    phase.phaseNumber = phaseNumber
-    phase.name = name
-    phase.description = description
-    phase.category = category
-    phase.dueDate = dueDate
-    phase.completed = completed
+        phase.userId = userId
+        phase.projectId = projectId
+        phase.phaseNumber = phaseNumber
+        phase.name = name
+        phase.description = description
+        phase.category = category
+        phase.dueDate = dueDate
+        phase.completed = completed
 
-    db.session.commit()
-    return phase_schema.jsonify(phase)
+        db.session.commit()
+        return phase_schema.jsonify(phase)
+    except:
+        raise AssertionError('Something went wrong')
 
 @app.route('/phase/delete/<id>', methods=['DELETE'])
 @verify
 def deletePhase(user, id):
-    phase = Phase.query.get(id)
-    db.session.delete(phase)
-    db.session.commit()
+    try:
+        phase = Phase.query.get(id)
+        db.session.delete(phase)
+        db.session.commit()
 
-    return phase_schema.jsonify(phase)
+        return phase_schema.jsonify(phase)
+    except:
+        raise AssertionError('Something went wrong')
