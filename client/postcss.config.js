15a1f065046ee219d6d9811e4352d555160e5541
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-26 15:17:09 -0400

    Fixed sidebar

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -3,6 +3,6 @@ const tailwindcss = require('tailwindcss')
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
     ]
 }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,11 @@ function App() {
     <div className="App">
       <div className="flex">
         <Sidebar />
-        <div className="flex-1 p-4 bg-gray-200">
-        
+        <div className="flex-1 p-4 bg-white">
+          <p className="text-6xl font-semibold mt-12">Projects</p>
+          <div className="w-max-sm bg-gray-200 rounded-lg h-max-sm h-screen">
+            test
+          </div>
         </div>
       </div>
     </div>
