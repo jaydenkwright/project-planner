cbe88b405bcd6d2354c930cfbcb7e59627b72ab4
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-25 20:59:13 -0400

    Setup tailwindcss

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,7 +2,7 @@ const tailwindcss = require('tailwindcss')
 
 module.exports = {
     plugins: [
-        tailwindcss('./tailwindjs'),
+        tailwindcss('./tailwind.js'),
         require('autoprefixer')
     ]
 }
