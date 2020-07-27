3429781c44aedec22e42fe7e23c81ebb11f9ce7c
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-27 19:40:44 -0400

    Implemented react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Sidebar from './components/Sidebar'
 import Projects from './components/Projects'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
@@ -8,7 +14,16 @@ function App() {
       <div className="flex">
         <Sidebar />
         <div className="flex-1 bg-white">
-          <Projects />
+          <Router>
+            <Switch>
+              <Route path='/' exact>
+                <Projects />
+              </Route>
+              <Route path='/create' exact>
+                
+              </Route>
+            </Switch>
+          </Router>
         </div>
       </div>
     </div>
