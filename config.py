f8115e8f940cd8058bd9c8f4d4862da455712eac
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-27 00:25:22 -0400

    added cors config

--- config.py
+++ config.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from settings.settings import DATABASE
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret'
