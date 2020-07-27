4635ef9ab3d2190ebca158d4b046ecde8c88d98f
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-26 22:03:26 -0400

    Seperated elements into ProjectCard and NewProjectCard components

--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import ProjectCard from './ProjectCard'
+import NewProjectCard from './NewProjectCard'
 
 export const Projects: React.FC =() => {
     return (
         <div>
             <Header title="Projects"/>
             <div className="flex flex-wrap w-max-sm bg-gray-200 rounded-lg h-max-sm h-screen">
-                <div className="bg-white w-48 h-48 rounded-lg mx-12 mt-10">
-                    
-                </div> 
-                <div className="bg-white w-48 h-48 rounded-lg mx-12 mt-10">
-                    
-                </div> 
-                <div className="bg-white w-48 h-48 rounded-lg mx-12 my-10">
-                    
-                </div> 
+                <ProjectCard data={{title: 'Social Media App', description: 'A social media app built with a React.JS frontend and a flask backend', category: 'Web Devlopment'}}/> 
+                <NewProjectCard />
             </div>
         </div>
     )
