7e349b1f9bd480244fe8078b057955a6b475df10
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-26 23:54:41 -0400

    Fixed issue with height

--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -5,10 +5,13 @@ import NewProjectCard from './NewProjectCard'
 
 export const Projects: React.FC =() => {
     return (
-        <div>
+        <div className="">
             <Header title="Projects"/>
-            <div className="flex flex-wrap w-max-sm bg-gray-200 rounded-lg h-max-sm h-screen">
+            <div className="flex flex-wrap w-max-sm bg-gray-200 rounded-lg h-max-sm pb-64">
                 <ProjectCard data={{title: 'Social Media App', description: 'A social media app built with a React.JS frontend and a flask backend', category: 'Web Devlopment'}}/> 
+                <ProjectCard data={{title: 'Social Media App', description: 'A social media app built with a React.JS frontend and a flask backend', category: 'Web Devlopment'}}/> 
+                <ProjectCard data={{title: 'Social Media App', description: 'A social media app built with a React.JS frontend and a flask backend', category: 'Web Devlopment'}}/> 
+                <ProjectCard data={{title: 'Social Media App', description: 'A social media app built with a React.JS frontend and a flask backend', category: 'Web Devlopment'}}/>
                 <NewProjectCard />
             </div>
         </div>
