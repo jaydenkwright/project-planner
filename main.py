b0e3e0c5eb036bd83a46b76ff568fc14a2036825
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-27 00:24:47 -0400

    Added flask-cors to allow the react frontend to make requests

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ import PhaseRoute
 import TaskRoute
 import UserRoute
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
