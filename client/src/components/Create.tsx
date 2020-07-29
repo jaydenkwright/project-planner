c83907b73b5f1a0540654b58dbb59d11dc84adbe
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-29 14:12:19 -0400

    Created form for users to create new projects

--- client/src/components/Create.tsx
+++ client/src/components/Create.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import Header from './Header'
 import Layout from './Layout'
+import moment from 'moment'
 
 export default function Create() {
     return (
         <div>
            <Header title='Create'/> 
             <Layout>
-                <form>
-                    <input type='text' />
-                </form> 
+                <div className='mx-auto py-16'>
+                    <form>
+                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Name</label>
+                        <input type='text' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100}/>
+                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Description</label>
+                        <textarea className='w-64 h-20 rounded-lg py-2 px-2 shadow-sm outline-none focus:shadow-outline block' />
+                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Category</label>
+                        <select className='w-64 px-2 py-2 rounded-lg shadow-sm outline-none focus:shadow-outline block'>
+                            <option value='Programming'>Programming</option>
+                            <option value='Web devlopment'>Web devlopment</option>
+                            <option value='Design'>Design</option>
+                            <option value='Writing'>Writing</option>
+                            <option value='Architecture'>Architecture</option>
+                            <option value='Marketing'>Marketing</option>
+                            <option value='Art'>Art</option>
+                            <option value='Video'>Video</option>
+                        </select>
+                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Due By</label>
+                        <input type='date' className='w-64 px-2 py-2 rounded-lg shadow-sm outline-none focus:shadow-outline block'/>
+                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Privacy</label>
+                        <select className='w-64 px-2 py-2 rounded-lg shadow-sm outline-none focus:shadow-outline block'>
+                            <option value='Private'>Private</option>
+                            <option value='Public'>Public</option>
+                        </select>
+                        <input type='submit' className='block px-2 py-2 mt-2 rounded-lg bg-green-500 text-white shadow-sm font-semibold'/>
+                    </form> 
+                </div>
            </Layout>
         </div>
     )
