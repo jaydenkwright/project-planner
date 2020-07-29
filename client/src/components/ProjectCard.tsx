5722a863b704426f8477fb4ecd8ac6ab08315334
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-29 17:44:39 -0400

    Project cards now link to the project page of the specific project

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Props } from './Interfaces/ProjectInterface'
+import { useHistory } from "react-router-dom";
 
 export const ProjectCard: React.FC<Props> = ({ data }) => {
+    const history = useHistory()
     return (
-        <div className="flex-initial m-10 bg-white h-56 w-56 rounded-lg p-4 shadow-md">
+        <div className="flex-initial m-10 bg-white h-56 w-56 rounded-lg p-4 shadow-md" onClick={() => history.push(`/projects/${data.id}`)}>
             <p className="text-gray-900 font-semibold truncate">{data.name}</p>
             <p className="text-gray-600 font-medium mt-2">{data.description}</p>
             <div className="px-2 bg-red-500 rounded-full text-center shadow-sm">
