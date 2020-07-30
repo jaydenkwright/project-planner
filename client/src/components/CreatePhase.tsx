8817d117317e05d5a1adeff302b24a3969f0b7e5
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-30 17:55:36 -0400

    Created CreatePhase component

---
+++ client/src/components/CreatePhase.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import Layout from './Layout'
+
+export const CreatePhase: React.FC = () => {
+    return (
+        <div>
+            <Header title='Create'/>
+            <Layout>
+                
+            </Layout>
+            
+        </div>
+    )
+}
+
+export default CreatePhase
