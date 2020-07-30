8897b833497173d0dfeee496f2e62d36d0e154e9
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-30 17:28:36 -0400

    Accidently deleted

--- client/src/components/NewProjectCard.tsx
+++ client/src/components/NewProjectCard.tsx
@@ -10,7 +10,7 @@ export default function NewProjectCard() {
         <div className="m-10 bg-white h-56 w-56 rounded-lg p-4 shadow-md" onClick={createNew}>
             <p className="text-gray-900 font-semibold text-xl text-center">Create New Project</p>
                 <span>
-                    <img className="w-24 h-24 m-auto mt-6 text-center" src={require('../assets/plus.svg')} />
+                    <img className="w-24 h-24 m-auto mt-6 text-center" src={require('../assets/plus.svg')} alt='Create new project'/>
                 </span>
         </div>
     )
