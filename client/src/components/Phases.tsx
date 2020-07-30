36e2000936d358c5792807ac29cbd8587026b07b
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-30 17:22:00 -0400

    Created component to display a specific project's phases

---
+++ client/src/components/Phases.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Props } from './Interfaces/PhaseInterface'
+
+const Phases: React.FC<Props> = ({ data }) => {
+    return (
+        <div className='flex-initial bg-white w-64 p-4 rounded-lg m-10'>
+            <div className='text-2xl font-semibold text-gray-900'>
+                {data.name}
+            </div>
+            <div className="bg-red-500 rounded-full shadow-sm inline-block px-2">
+                <p className="text-gray-100 font-medium">{data.category}</p>
+            </div>
+            <div className='mt-2 text-gray-700 text-xl'>
+                {data.description}
+            </div>
+        </div>
+    )
+}
+
+export default Phases
