216d895446f3485a44af265f2922ba846d83e9d3
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-30 21:30:49 -0400

    Created function to retrieve tasks from backend

--- client/src/components/Tasks.tsx
+++ client/src/components/Tasks.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CreateTask from './CreateTask'
 import Task from './Task'
+import { TaskInterface } from './Interfaces/TaskInterface'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 export const Tasks = () => {
+    const [tasks, setTasks] = useState<TaskInterface[]>()
+    const { id } = useParams()
+
+    useEffect(() => {
+        const getTasks = async () => {
+            const res = await axios.get(`http://localhost:5000/tasks/${id}`, { withCredentials: true })
+            setTasks(res.data)
+        }
+        getTasks()
+    }, [])
     return (
         <div className='flex p-4 w-full'>
             <div className='w-1/3 h-screen bg-white mx-4 rounded-lg p-4'>
@@ -10,21 +23,21 @@ export const Tasks = () => {
                     To do
                 </div>
                 <CreateTask />
-                <Task />
-                <Task />
-                <Task />
+                {tasks ?
+                    tasks.map((task) => (
+                        <Task data={task} />
+                    )) 
+                : null}
             </div>
             <div className='w-1/3 h-screen bg-white mx-4 rounded-lg p-4 shadow-sm'>
                 <div className='text-xl text-text-gray-800'>
                     In progress
                 </div>
-                <Task />
             </div>
             <div className='w-1/3 h-screen bg-white mx-4 rounded-lg p-4 shadow-sm'>
                 <div className='text-xl text-text-gray-800'>
                     Completed
                 </div>
-                <Task />
             </div>
         </div>
     )
