55da25e2724799c3ce362003b8d42bc881d58c73
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-30 21:05:10 -0400

    Create component to display tasks

---
+++ client/src/components/Task.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Task = () => {
+    return (
+        <div className='text-lg text-gray-900 py-3 px-2 bg-gray-100 mt-2 rounded-lg'>
+            Create design mock-ups in figma for user profiles
+        </div>
+    )
+}
+
+export default Task
