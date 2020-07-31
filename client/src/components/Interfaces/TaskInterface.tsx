ca019dd49e43e6fade4b606f63637aa9993dc503
jaydenkwright <jaydenkwright3@gmail.com>
2020-07-30 21:28:12 -0400

    Created interfaces for tasks

---
+++ client/src/components/Interfaces/TaskInterface.tsx
@@ -0,0 +1,28 @@
+export interface TaskProps{
+    data: {
+        id: Number,
+        userId: string,
+        phaseId: Number,
+        taskText: string,
+        completed: boolean,
+        date: String
+        }
+    }
+
+export interface TaskInterface {
+    id: Number,
+    userId: string,
+    phaseId: Number,
+    taskText: string,
+    completed: boolean,
+    date: String
+}
+
+const init = {
+        completed: false,
+        date: "",
+        id: 0,
+        phaseId: 0,
+        taskText: "",
+        userId: ""
+}
