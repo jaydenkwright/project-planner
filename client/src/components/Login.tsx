19994eefbf29d27db41fd9207fe0cf3f3d55c9ce
jaydenkwright <jaydenkwright3@gmail.com>
2020-08-04 13:58:33 -0400

    User login info will now be sent to server with login() function and a cookie will be sent to the client

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const submit = (e: any) => {
+        e.preventDefault()
+        const login = async () => {
+            const res = await axios.post('http://localhost:5000/login', {
+                email,
+                password
+            }, { withCredentials: true })
+            console.log(res)
+        }
+        login()
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={submit}>
                 <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Email</label>
                 <input type='email' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} required onChange={(e) => setEmail(e.target.value)}/>
                 <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Password</label>
