da045ba69a20b896af03ad2b839c68bca0bafba3
jaydenkwright <jaydenkwright3@gmail.com>
2020-08-30 16:45:29 -0400

    Created handleDragStart() and handleDragEnd() to deal with drag and drop functionality

--- client/src/components/Tasks.tsx
+++ client/src/components/Tasks.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import CreateTask from './CreateTask'
 import Task from './Task'
 import { TaskInterface } from './Interfaces/TaskInterface'
@@ -7,15 +7,36 @@ import axios from 'axios'
 
 export const Tasks = () => {
     const [tasks, setTasks] = useState<TaskInterface[]>()
+    const [dragging, setDragging] = useState<boolean>(false)
     const { id } = useParams()
+    const dragItem = useRef<any>()
+    const dragNode = useRef<any>()
 
     useEffect(() => {
         const getTasks = async () => {
             const res = await axios.get(`http://localhost:5000/tasks/${id}`, { withCredentials: true })
+            console.log(res.data)
             setTasks(res.data)
         }
         getTasks()
     }, [])
+
+    const handleDragStart = (e: any, params: any) => {
+        dragItem.current = params
+        dragNode.current = e.target
+        dragNode.current?.addEventListener('dragend', handleDragEnd)
+        setTimeout(() => {
+            setDragging(true)
+        }, 0)
+    }
+
+    const handleDragEnd = () => {
+        setDragging(false)
+        dragNode.current?.removeEventListener('dragend', handleDragEnd)
+        dragItem.current = null
+        dragNode.current = null
+    }
+
     return (
         <div className='flex p-4 w-full'>
             <div className='w-1/3 h-screen bg-white mx-4 rounded-lg p-4'>
@@ -25,7 +46,7 @@ export const Tasks = () => {
                 <CreateTask setTasks={setTasks} tasks={tasks ? tasks : []}/>
                 {tasks ?
                     tasks.map((task) => (
-                        <Task data={task} />
+                        <Task data={task} handleDragStart={handleDragStart} dragging={dragging}/>
                     )) 
                 : null}
             </div>
