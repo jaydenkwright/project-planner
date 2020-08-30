1e99772a4ba53f41683541fc05036ea0ff03a201
jaydenkwright <jaydenkwright3@gmail.com>
2020-08-30 16:01:21 -0400

    Made div a draggable element

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { TaskProps } from './Interfaces/TaskInterface'
 
-const Task: React.FC<TaskProps> = ({ data }) => {
+const Task: React.FC<TaskProps | any> = ({ data, handleDragStart }) => {
     return (
-        <div className='text-lg text-gray-900 py-3 px-2 bg-gray-100 mt-2 rounded-lg'>
+        <div draggable onDragStart={(e) => handleDragStart(e, {id: data.id, completed: data.completed})} className='text-lg text-gray-900 py-3 px-2 bg-gray-100 mt-2 rounded-lg'>
             {data.taskText}
         </div>
     )
