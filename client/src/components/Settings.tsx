06b7dff04ee01d8f57b70ab8c6680076441a6096
jaydenkwright <jaydenkwright3@gmail.com>
2020-08-31 20:09:59 -0400

    Created settings component

---
+++ client/src/components/Settings.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Layout from './Layout'
+
+const Settings = () => {
+    return (
+        <div>
+            <Header title='Settings' />
+            <Layout>
+
+            </Layout>
+        </div>
+    )
+}
+
+export default Settings
