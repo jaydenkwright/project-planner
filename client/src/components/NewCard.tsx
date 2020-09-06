ee336ce5f9f86299a777c920bce3ccbcafdb2164
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-06 19:40:03 -0400

    Updated class names

--- client/src/components/NewCard.tsx
+++ client/src/components/NewCard.tsx
@@ -7,10 +7,10 @@ export const NewCard: React.FC<{title: String, destination: String}> = ({ title,
         history.push(`/${destination}`)
     }
     return (
-        <div className="m-10 bg-white h-56 w-56 rounded-lg p-4 shadow-md" onClick={createNew}>
-            <p className="text-gray-900 font-semibold text-xl text-center">{title}</p>
+        <div className="newCardContainer" onClick={createNew}>
+            <p className="newCardTitle">{title}</p>
                 <span>
-                    <img className="w-24 h-24 m-auto mt-6 text-center" src={require('../assets/plus.svg')} alt='Create new project'/>
+                    <img className="newCardImage" src={require('../assets/plus.svg')} alt='Create new project'/>
                 </span>
         </div>
     )
