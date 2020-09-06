424004e6218a272fa0c75f4d460f1cc91eb4bb68
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-06 19:37:06 -0400

    Updated class names

--- client/src/components/PhaseCard.tsx
+++ client/src/components/PhaseCard.tsx
@@ -5,14 +5,14 @@ import { useHistory } from "react-router-dom";
 const PhaseCard: React.FC<Props> = ({ data }) => {
     const history = useHistory()
     return (
-        <div className='flex-initial bg-white w-64 p-4 rounded-lg m-10' onClick={() => history.push(`/phase/${data.id}`)}>
-            <div className='text-2xl font-semibold text-gray-900'>
+        <div className='phaseCardContainer' onClick={() => history.push(`/phase/${data.id}`)}>
+            <div className='phaseName'>
                 {data.name}
             </div>
-            <div className="bg-red-500 rounded-full shadow-sm inline-block px-2">
-                <p className="text-gray-100 font-medium">{data.category}</p>
+            <div className="phaseCategoryContainer">
+                <p className="phaseCategory">{data.category}</p>
             </div>
-            <div className='mt-2 text-gray-700 text-xl'>
+            <div className='phaseDescription'>
                 {data.description}
             </div>
         </div>
