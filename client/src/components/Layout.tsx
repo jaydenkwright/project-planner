09868b1281fc4a7d2322492c86b0e4e357ebd10e
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-07 13:58:33 -0400

    Update class names

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Layout: React.FC = ({ children }) => {
     return (
-        <div className="layout">
+        <div className="layout w-max-sm max-h-full">
             {children}
         </div>
     )
