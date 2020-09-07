c34b54eb973dfeb9d4d9238fc4f6adaa15cd257d
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-07 12:31:03 -0400

    Updated class names

--- client/src/components/Registration.tsx
+++ client/src/components/Registration.tsx
@@ -25,19 +25,19 @@ const Registration = () => {
             <form onSubmit={submit}>
                 <div className='flex'>
                     <div className='flex flex-col'>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>First Name</label>
-                        <input type='text' className='rounded-lg mb-1 py-2 px-2 w-32 shadow-sm outline-none focus:shadow-outline block' maxLength={50} required onChange={(e) => setFirstName(e.target.value)}/>
+                        <label className='textboxLabel'>First Name</label>
+                        <input type='text' className='textbox' maxLength={50} required onChange={(e) => setFirstName(e.target.value)}/>
                     </div>
                     <div className='flex flex-col mx-2'>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Last Name</label>
-                        <input type='text' className='rounded-lg mb-1 py-2 px-2 w-32 shadow-sm outline-none focus:shadow-outline block' maxLength={50} required onChange={(e) => setLastName(e.target.value)}/>
+                        <label className='textboxLabel'>Last Name</label>
+                        <input type='text' className='textbox' maxLength={50} required onChange={(e) => setLastName(e.target.value)}/>
                     </div>
                 </div>
-                <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Email</label>
-                <input type='email' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} required onChange={(e) => setEmail(e.target.value)}/>
-                <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Password</label>
-                <input type='password' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} required onChange={(e) => setPassword(e.target.value)}/>
-                <input type='submit' className='block px-2 py-2 mt-2 rounded-lg bg-green-500 text-white shadow-sm font-semibold'/>
+                <label className='textboxLabel'>Email</label>
+                <input type='email' className='textbox' maxLength={100} required onChange={(e) => setEmail(e.target.value)}/>
+                <label className='textboxLabel'>Password</label>
+                <input type='password' className='textbox' maxLength={100} required onChange={(e) => setPassword(e.target.value)}/>
+                <input type='submit' className='submitBtn'/>
             </form>
         </div>
     )
