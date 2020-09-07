dd582ded6cc39c38ae66e813e40c98d33a47ddea
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-07 12:45:31 -0400

    Updated class names

--- client/src/components/CreateProject.tsx
+++ client/src/components/CreateProject.tsx
@@ -44,12 +44,12 @@ const CreateProject: React.FC = () => {
                 <div className='mx-auto py-16'>
                     { error ? <Error error={error}/> : null}
                     <form onSubmit={onSubmit}>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Name</label>
-                        <input type='text' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} required onChange={(e) => setName(e.target.value)}/>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Description</label>
-                        <textarea className='w-64 h-20 rounded-lg py-2 px-2 shadow-sm outline-none focus:shadow-outline block' onChange={(e) => setDescription(e.target.value)} maxLength={200}/>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Category</label>
-                        <select className='w-64 px-2 py-2 rounded-lg shadow-sm outline-none focus:shadow-outline block' value={category} required onChange={(e) => setCategory(e.target.value)}>
+                        <label className='textboxLabel'>Name</label>
+                        <input type='text' className='textbox' maxLength={100} required onChange={(e) => setName(e.target.value)}/>
+                        <label className='textboxLabel'>Description</label>
+                        <textarea className='descriptionTextArea' onChange={(e) => setDescription(e.target.value)} maxLength={200}/>
+                        <label className='textboxLabel'>Category</label>
+                        <select className='categorySelector' value={category} required onChange={(e) => setCategory(e.target.value)}>
                             <option value='Programming'>Programming</option>
                             <option value='Web devlopment'>Web devlopment</option>
                             <option value='Design'>Design</option>
@@ -59,14 +59,14 @@ const CreateProject: React.FC = () => {
                             <option value='Art'>Art</option>
                             <option value='Video'>Video</option>
                         </select>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Due By</label>
-                        <input type='date' className='w-64 px-2 py-2 rounded-lg shadow-sm outline-none focus:shadow-outline block'onChange={(e) => setDueDate(e.target.value)}/>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Privacy</label>
-                        <select className='w-64 px-2 py-2 rounded-lg shadow-sm outline-none focus:shadow-outline block' value={privacy} onChange={(e) => setPrivacy(e.target.value)}>
+                        <label className='textboxLabel'>Due By</label>
+                        <input type='date' className='dateSelector'onChange={(e) => setDueDate(e.target.value)}/>
+                        <label className='textboxLabel'>Privacy</label>
+                        <select className='categorySelector' value={privacy} onChange={(e) => setPrivacy(e.target.value)}>
                             <option value='Private'>Private</option>
                             <option value='Public'>Public</option>
                         </select>
-                        <input type='submit' className='block px-2 py-2 mt-2 rounded-lg bg-green-500 text-white shadow-sm font-semibold'/>
+                        <input type='submit' className='submitBtn'/>
                     </form> 
                 </div>
            </Layout>
