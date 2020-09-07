be9b757b5a4b8ac06df48550fbaeb16f44ffb6ce
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-07 12:27:04 -0400

    Updated class names

--- client/src/components/Settings.tsx
+++ client/src/components/Settings.tsx
@@ -3,9 +3,10 @@ import { useHistory } from "react-router-dom";
 import { UserInterface } from './Interfaces/UserInterface'
 import Header from './Header'
 import Layout from './Layout'
+import Error from './Error'
 import axios from 'axios'
 
-const Settings = () => {
+const Settings: React.FC = () => {
     const history = useHistory()
     const [user, setUser] = useState<UserInterface>()
     const [firstName, setFirstName] = useState<string>('')
@@ -41,11 +42,7 @@ const Settings = () => {
                     history.push('/')
                 }
             }catch(err){
-                if(err.response.status === 401){
-                    setError('Incorrect Password')
-                }else{
-                    setError('Something went wrong')
-                }
+                setError(err.response.data.msg)
             }
         }
         updateUser()
@@ -54,20 +51,18 @@ const Settings = () => {
         <div>
             <Header title='Settings' />
             <Layout>
-            <div className='mx-auto py-16'>
-                    { error ? <div className='text-gray-100 px-1 font-semibold bg-red-500 rounded-lg'>
-                        {error}
-                    </div> : null}
+                <div className='mx-auto py-16'>
+                    { error ? <Error error={error}/> : null}
                     <form onSubmit={submit}>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>First Name</label>
-                        <input placeholder={user?.firstName} type='text' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} onChange={(e) => setFirstName(e.target.value)}/>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Last Name</label>
-                        <input placeholder={user?.lastName} type='text' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} onChange={e => setLastName(e.target.value)} />
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Email</label>
-                        <input placeholder={user?.email} type='text' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} onChange={e => setEmail(e.target.value)}/>
-                        <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Password</label>
-                        <input type='password' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} required onChange={e => setPassword(e.target.value)}/>
-                        <input type='submit' className='block px-2 py-2 mt-2 rounded-lg bg-green-500 text-white shadow-sm font-semibold'/>
+                        <label className='textboxLabel'>First Name</label>
+                        <input placeholder={user?.firstName} type='text' className='textbox' maxLength={100} onChange={(e) => setFirstName(e.target.value)}/>
+                        <label className='textboxLabel'>Last Name</label>
+                        <input placeholder={user?.lastName} type='text' className='textbox' maxLength={100} onChange={e => setLastName(e.target.value)} />
+                        <label className='textboxLabel'>Email</label>
+                        <input placeholder={user?.email} type='text' className='textbox' maxLength={100} onChange={e => setEmail(e.target.value)}/>
+                        <label className='textboxLabel'>Password</label>
+                        <input type='password' className='textbox' maxLength={100} required onChange={e => setPassword(e.target.value)}/>
+                        <input type='submit' className='submitBtn'/>
                     </form> 
                 </div>
             </Layout>
