57bebde561ba925a9b464c811a39545b64cb9f4c
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-07 12:34:52 -0400

    Updated class name

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Layout: React.FC = ({ children }) => {
     return (
-        <div className="flex flex-wrap w-max-sm bg-gray-200 rounded-lg h-max-sm pb-64">
+        <div className="layout">
             {children}
         </div>
     )
