cc57f0d1d2c93e4ce0dee220470ea55187d9bc0c
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-07 12:34:22 -0400

    Updated class names

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -19,11 +19,11 @@ const Login = () => {
     return (
         <div>
             <form onSubmit={submit}>
-                <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Email</label>
-                <input type='email' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} required onChange={(e) => setEmail(e.target.value)}/>
-                <label className='block mt-1 mb-1 text-gray-700 px-1 font-semibold'>Password</label>
-                <input type='password' className='rounded-lg mb-1 py-2 px-2 w-64 shadow-sm outline-none focus:shadow-outline block' maxLength={100} required onChange={(e) => setPassword(e.target.value)}/>
-                <input type='submit' className='block px-2 py-2 mt-2 rounded-lg bg-green-500 text-white shadow-sm font-semibold'/>
+                <label className='textboxLabel'>Email</label>
+                <input type='email' className='textbox' maxLength={100} required onChange={(e) => setEmail(e.target.value)}/>
+                <label className='textboxLabel'>Password</label>
+                <input type='password' className='textbox' maxLength={100} required onChange={(e) => setPassword(e.target.value)}/>
+                <input type='submit' className='submitBtn'/>
             </form>
         </div>
     )
