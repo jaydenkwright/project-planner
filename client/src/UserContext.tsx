4c8a489b0c4fe936ef8430fda8a5d72032b12805
jaydenkwright <jaydenkwright3@gmail.com>
2020-09-16 19:42:17 -0400

    Create context for user logged in state

---
+++ client/src/UserContext.tsx
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react'
+
+const UserContext = React.createContext({})
+
+export const UserProvider = UserContext.Provider
+export const UserConsumer = UserContext.Consumer
+
+export default UserContext
