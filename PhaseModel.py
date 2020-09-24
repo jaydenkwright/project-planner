8fa9d0e6c725a0199d5e720c568b0c782001d40c
Jayden Wright <jaydenkwright3@gmail.com>
2020-09-23 20:49:12 -0400

    Creating Phase Database Tables

--- PhaseModel.py
+++ PhaseModel.py
@@ -37,7 +37,9 @@ class Phase(db.Model):
         if len(description) > 200:
             return {"msg": "Max character limit exceeded"}, 401
         return description
-        
+       
+db.create_all()
+print('created') 
 
 class PhaseSchema(marsh.Schema):
     class Meta:
