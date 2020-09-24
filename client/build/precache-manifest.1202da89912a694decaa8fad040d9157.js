7c9aad8b3f76c3c7fa70f746942684f571718086
Jayden Wright <jaydenkwright3@gmail.com>
2020-09-23 21:47:07 -0400

    Fixed

---
+++ client/build/precache-manifest.1202da89912a694decaa8fad040d9157.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2771d8f4fb8f71c0eb3d12d05a2af6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56e7ec29c35d74fe83bb",
+    "url": "/static/css/main.2292b742.chunk.css"
+  },
+  {
+    "revision": "84e15494386c39e3306f",
+    "url": "/static/js/2.360a034f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.360a034f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e7ec29c35d74fe83bb",
+    "url": "/static/js/main.8c77179a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "e9a86709d98c68bc6399f61bf0159451",
+    "url": "/static/media/image1.e9a86709.svg"
+  },
+  {
+    "revision": "a69b1e05a161a6f60cef3beed13f4a98",
+    "url": "/static/media/plus.a69b1e05.svg"
+  }
+]);
