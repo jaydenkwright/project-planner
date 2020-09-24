83950391062d850bc7f3010203e85c3f61b74482
Jayden Wright <jaydenkwright3@gmail.com>
2020-09-23 20:45:06 -0400

    Fixed .gitignore

---
+++ settings/settings.py
@@ -0,0 +1,2 @@
+#DATABASE = 'postgresql://postgres:postgres@localhost/project_planner'
+DATABASE = 'postgres://uidfpkhlkxaepm:b60fe86141c51ff0b4c4b5863f8e166185d5209ad5fb0a47ca93fec218e5e57e@ec2-50-16-221-180.compute-1.amazonaws.com:5432/dak56uvl75geu3'
