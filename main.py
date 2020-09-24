6e94ea45f367d3873bf22b59c0ea0203006d290c
Jayden Wright <jaydenkwright3@gmail.com>
2020-09-23 21:08:04 -0400

    Fixed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import UserRoute
 
 @app.after_request
 def after_request(response):
-  #response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
