7f24a56f53dd58a2e67488809cf02e5c8852ff80
Jayden Wright <jaydenkwright3@gmail.com>
2020-09-26 00:27:13 -0400

    Fixed

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from settings.settings import DATABASE
