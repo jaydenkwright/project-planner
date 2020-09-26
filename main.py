7edb24a8fd79d8365a765d42fdfbc39cf688fffa
Jayden Wright <jaydenkwright3@gmail.com>
2020-09-26 00:05:03 -0400

    Fixed

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def catch_all(u_path):
     print('fuck')
     return u_path
 
-# @app.after_request
-# def after_request(response):
-#   response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
-#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-#   response.headers.add('Access-Control-Allow-Credentials', 'true')
-#   return response
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
