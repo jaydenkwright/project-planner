c71399aff20e9a331c320771e49436292ac14da3
Jayden Wright <jaydenkwright3@gmail.com>
2020-09-26 00:16:50 -0400

    Fixed

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import UserRoute
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>')
 def catch_all(u_path):
-    return app.send_static_file('index.html')
+    return app.send_static_file('./client/build/index.html')
 
 @app.after_request
 def after_request(response):
